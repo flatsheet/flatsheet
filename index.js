89cf0036ebea6b131d0b43a716b81fbd5ec591fe
sethvincent <sethvincent@gmail.com>
2014-07-15 17:51:20 -0700

    update to use new example/table-editor v0.2.0

---
+++ index.js
@@ -0,0 +1,52 @@
+var TableEditor = require('table-editor');
+var prettify = require('jsonpretty');
+var elClass = require('element-class');
+
+var data = {
+  headers: [
+    { name: 'example', type: 'string' }
+  ],
+  rows: [
+    { example: null },
+    { example: null },
+    { example: null }
+  ]
+};
+
+var editor = new TableEditor('main-content', data);
+
+var addRow = document.getElementById('add-row');
+addRow.addEventListener('click', function (e) {
+  editor.addRow();
+});
+
+var addColumn = document.getElementById('add-column');
+addColumn.addEventListener('click', function (e) {
+  var name = window.prompt('New column name');
+  editor.addColumn({ name: name, type: 'string' });
+});
+
+var codeBox = document.getElementById('code-box');
+var textarea = codeBox.querySelector('textarea');
+
+var showJSON = document.getElementById('show-json');
+showJSON.addEventListener('click', function (e) {
+  editor.getJSON(function (data) {
+    textarea.value = prettify(data);
+    elClass(codeBox).remove('hidden');
+  });
+});
+
+var showCSV = document.getElementById('show-csv');
+showCSV.addEventListener('click', function (e) {
+  editor.getCSV(function (data) {
+    textarea.value = data;
+    elClass(codeBox).remove('hidden');
+  });
+});
+
+var close = document.getElementById('close');
+close.addEventListener('click', function (e) {
+  textarea.value = '';
+  elClass(codeBox).add('hidden');
+});
