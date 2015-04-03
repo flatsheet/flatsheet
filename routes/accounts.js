84687e2c737fa6a168829bd84a710feeb6306ee0
Luke Swart <lukeswart@gmail.com>
2015-04-03 05:22:45 -0700

    Remove dynamic handler loading code

--- routes/accounts.js
+++ routes/accounts.js
@@ -8,73 +8,47 @@ exports.install = function (server, prefix) {
   */
 
   server.route(prefix, handler.getListOfAccounts.bind(handler));
-  //server.route(prefix, function(req, res) {
-  //  server.accounts.getListOfAccounts(req, res);
-  //});
 
   /*
   *  Sign in
   */
 
   server.route(prefix + '/sign-in', handler.signIntoAccount.bind(handler));
-  //server.route(prefix + '/sign-in', function(req, res) {
-  //  server.accounts.signIntoAccount(req, res);
-  //});
-
 
   /*
    *  Create an admin account (admin only)
    */
 
   server.route(prefix + '/create-admin', handler.createAdminAccount.bind(handler));
-  //server.route(prefix + '/create-admin', function(req, res) {
-  //  server.accounts.createAdminAccount(req, res);
-  //});
 
   /*
   *  Create an account
   */
 
   server.route(prefix + '/create', handler.createAccount.bind(handler));
-  //server.route(prefix + '/create', function(req, res) {
-  //  server.accounts.createAccount(req, res);
-  //});
 
   /*
    * Delete an account (admin only)
    */
 
   server.route(prefix + '/delete/:username', handler.deleteAccount.bind(handler));
-  //server.route(prefix + '/delete/:username', function(req, res, opts) {
-  //  server.accounts.deleteAccount(req, res, opts);
-  //});
 
   /*
   * Update an account
   */
 
   server.route(prefix + '/update/:username', handler.updateAccount.bind(handler));
-  //server.route(prefix + '/update/:username', function(req, res, opts) {
-  //  server.accounts.updateAccount(req, res, opts);
-  //});
 
   /*
   * Invite users to create accounts
   */
 
   server.route(prefix + '/invite', handler.invite.bind(handler));
-  //server.route(prefix + '/invite', function(req, res) {
-  //  server.accounts.invite(req, res);
-  //});
-
 
   /*
   * Accept invitation
   */
 
   server.route(prefix + '/accept', handler.acceptInvite.bind(handler));
-  //server.route(prefix + '/accept', function(req, res) {
-  //  server.accounts.acceptInvite(req, res);
-  //});
 
 };
