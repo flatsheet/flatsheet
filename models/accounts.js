a517418a5be69abb90e7a2a8f2b858cd2f8e5c25
sethvincent <sethvincent@gmail.com>
2015-06-08 00:09:29 -0700

    create models/accounts.js

---
+++ models/accounts.js
@@ -0,0 +1,25 @@
+var sublevel = require('subleveldown')
+
+module.exports = function (db) {
+  function accountdownBasic (db, prefix) {
+    return require('accountdown-basic')(db, prefix, { key: 'key' })
+  }
+
+  var accountdown = require('accountdown')(sublevel(db, 'accounts'), {
+    login: { basic: accountdownBasic }
+  })
+
+  var accounts = require('accountdown-basic')(accountdown, { 
+    db: db,
+    properties: {
+      username: { type: 'string' },
+      email: { type: 'string' },
+      profile: { type: 'string' }
+    },
+    required: ['username', 'email', 'profile'],
+    indexKeys: ['username', 'email', 'profile']
+  })
+
+  return accounts
+}
+
